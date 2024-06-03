4b577531e386ec45c501420d78290d1359cea5f9
Emil Wide <emil.wide@aros.se>
2024-06-03 13:00:31 +0200

    Add check for RAM used by process

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ import matplotlib.pyplot as plt
 
 
 def bytes_to_gb(nof_bytes: int) -> float:
+    """Convert bytes to GB."""
     return round(nof_bytes / math.pow(2, 30), 4)
 
 
 def get_ram_usage() -> float:
+    """Return the current RAM usage of the system in GB."""
     ram_info = psutil.virtual_memory()
     return bytes_to_gb(ram_info.used)
 
 
 def get_total_ram() -> float:
+    """Return the amount of RAM available on the system in GB."""
     return bytes_to_gb(psutil.virtual_memory().total)
 
 
 def get_rss() -> float:
+    """Return the amount of RAM used by the running script in GB."""
     process = psutil.Process(os.getpid())
     return bytes_to_gb(process.memory_info().rss)
+
+
+def get_available_ram(safety_margin_percent: int = 10) -> float:
+    """Return the amount of RAM available in GB."""
+    safety_margin = 1 - (safety_margin_percent / 100)
+    return round((get_total_ram() - get_ram_usage()) * safety_margin, 4)
 
 
 def main() -> None:
@@ -32,12 +42,18 @@ def main() -> None:
     dummy_data = list()
     ram_usage_lim = get_total_ram() * 0.95
     start_time = time.perf_counter()
+    ram_usage_start = get_ram_usage()
+    ram_remaining = True
+    ram_available = get_available_ram()
+    print(ram_available)
 
-    for _ in range(1024):
+    while ram_remaining:
         ram_usage.append(get_ram_usage())
         rss.append(get_rss())
         timestamps.append(time.perf_counter() - start_time)
         dummy_data.append([x for x in range(10000)])
+        if rss[-1] >= ram_available:
+            ram_remaining = False
 
     fig, ax = plt.subplots()
     assert isinstance(fig, Figure)
@@ -45,11 +61,12 @@ def main() -> None:
     fig: Figure
     ax: Axes
 
-    plt.axhline(ram_usage_lim, label="RAM limit (GB)", color="r", linestyle="dashed")
+    plt.axhline(ram_usage_lim, label=f"RAM limit: {round(ram_usage_lim, 2)} (GB)", color="r", linestyle="dashed")
     ax.plot(timestamps, ram_usage, label="Total RAM usage (GB)", color="k")
     ax.plot(timestamps, rss, label="Process RAM usage (GB)", color="y")
     ax.plot(timestamps[rss.index(max(rss))], max(rss), "yo", label=f"Max process RAM usage: {max(rss)} GB")
-    ax.plot(timestamps[ram_usage.index(max(ram_usage))], max(ram_usage), "ko", label=f"Max total RAM usage: {max(ram_usage)} GB")
+    ax.plot(timestamps[ram_usage.index(max(ram_usage))], max(ram_usage), "ro", label=f"Max total RAM usage: {max(ram_usage)} GB")
+    ax.plot(timestamps[0], ram_usage_start, "go", label=f"RAM usage at start: {ram_usage_start} GB")
     ax.grid()
     ax.set_title("Resource utilization: RAM")
     ax.set_ylabel("RAM usage (GB)")
